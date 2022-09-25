746aba38774f3d37cacd132325fe3b01bca521c9
Ngapa <krilinamar@gmail.com>
2022-09-25 09:06:48 +0700

    fix coba

--- bps/admin.py
+++ bps/admin.py
@@ -58,8 +58,8 @@ class IpmModelAdmin(ImportExportModelAdmin):
 
 @admin.register(Penduduk)
 class PendudukModelAdmin(ImportExportModelAdmin):
-    # list_display = ("tanggal", "jenis_kelamin")
-    # list_filter = ('jenis_kelamin')
+    list_display = ("tanggal", "jenis_kelamin")
+    list_filter = ('jenis_kelamin')
 
     class Meta:
         model = Penduduk
@@ -68,8 +68,8 @@ class PendudukModelAdmin(ImportExportModelAdmin):
 
 @admin.register(PendudukKecamatan)
 class PendudukKecamatanModelAdmin(ImportExportModelAdmin):
-    # list_display = ("tanggal", "kecamatan", "jenis_kelamin")
-    # list_filter = ('kecamatan')
+    list_display = ("tanggal", "kecamatan", "jenis_kelamin")
+    list_filter = ('kecamatan')
 
     class Meta:
         model = PendudukKecamatan
