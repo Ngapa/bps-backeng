76ce97049b7d30fcc02f94a99975f534b1804d5b
Ngapa <krilinamar@gmail.com>
2022-10-06 13:20:43 +0700

    fix adminurl

--- backend_oss/settings.py
+++ backend_oss/settings.py
@@ -4,10 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!c#5l-dh#npl84#s3pshcuvi$kps$$or#v^2ki=sg36j&-w_c0'
 
